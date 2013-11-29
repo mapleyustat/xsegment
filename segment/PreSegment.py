9e89d3c7e3713fa62a7896093a585deb7bc1fe93
lixuze <aihai2800@126.com>
2013-11-29 11:14:56 +0800

    解决 编码出现 UnicodeEncodeError: 'ascii' codec can't encode character 错误

--- segment/PreSegment.py
+++ segment/PreSegment.py
@@ -1,29 +1,36 @@
-# -*- coding: UTF-8 -*-
-
+# coding=utf-8
+#!/usr/bin/env python
 
 import re
-#import filetutil
-REGX_ARRY= [('URL' , ur"((https?|ftp|news):\/\/)?([a-z]([a-z0-9\-]*[\.。])+([a-z]*)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&]*)?)?(#[a-z][a-z0-9_]*)?"),
-            ('SIGN',r"[\.\=、，!！_?？\[\]【】：:。“”…\"〜]+"),
-            ('AT',ur"(//)?@[\u4e00-\u9fa5a-z0-9]+"),
-            ('FLOAT',ur"\d+\.\d+"),
-            ('INTEGER',ur"[0-9]+"),
-            ('ZN',ur"[\u4e00-\u9fa5]+"),
-            ('EN',ur"[a-zA-Z]+")]
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+
+REGX_ARRY = [(
+    'URL', ur"((https?|ftp|news):\/\/)?([a-z]([a-z0-9\-]*[\.。])+([a-z]*)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&]*)?)?(#[a-z][a-z0-9_]*)?"),
+    ('SIGN', r"[\.\=、，!！_?？\[\]【】：:。“”…\"〜]+"),
+    ('AT', ur"(//)?@[\u4e00-\u9fa5a-z0-9]+"),
+    ('FLOAT', ur"\d+\.\d+"),
+    ('INTEGER', ur"[0-9]+"),
+    ('ZN', ur"[\u4e00-\u9fa5]+"),
+    ('EN', ur"[a-zA-Z]+")]
 
 #WORD_EXTRACT_REGX =ur"(?P<URL>http://([a-z]{1,}\.){2}[a-z]{1,}(/[a-z0-9]{1,}){0,}\.[a-z0-9]{1,})"+ur"|(?P<SIGN>[\.\+\*\=、。，“”\"]+)"+ur"|(?P<ZN>[\u4e00-\u9fa5]+)"+ur"|(?P<EN>([a-z])+)"+ur"|(?P<FLOAT>\d+\.\d+)"+ur"|(?P<INTEGER>[0-9]+)"
 
-#fileregx = [ regx.split("#")  for regx in filetutil.read_file_strip("wordregx.txt")]
-WORD_EXTRACT_REGX = ur'|'.join(ur'(?P<%s>%s)' % pair for pair in REGX_ARRY)
-WORD_EXTRACT = re.compile(WORD_EXTRACT_REGX,re.U).finditer
+# fileregx = [ regx.split("#")  for regx in
+# filetutil.read_file_strip("wordregx.txt")]
+WORD_EXTRACT_REGX = ur'|'.join(ur'(?P<%s>%s)' % (pair[0],pair[1]) for pair in REGX_ARRY)
+WORD_EXTRACT = re.compile(WORD_EXTRACT_REGX, re.U).finditer
 print WORD_EXTRACT_REGX
 
 
 def getWordSign(d):
     if isinstance(d, dict):
-        for _key,_val in d.items():
+        for _key, _val in d.items():
             if _val != None:
-                return (_val,_key)
+                return (_val, _key)
     return None
 
 
@@ -33,14 +40,11 @@ def token(words):
 
 
 if __name__ == "__main__":
-    statements ="""五一假期拼凑了[。]“一个Google Analytics自动标记工具，我在博客里对这个工具进行简单的功能介绍并分享给有需要的朋友们使用。文章结尾附有工具下载地址。希望帮助大家减轻Google Analytics代码定制时的工作量。
+    statements = """五一假期拼凑了[。]“一个Google Analytics自动标记工具，我在博客里对这个工具进行简单的功能介绍并分享给有需要的朋友们使用。文章结尾附有工具下载地址。希望帮助大家减轻Google Analytics代码定制时的工作量。
 
 Read more: http://bluewhale.cc/tag/google-analytics#ixzz2cHtxfYjR""".decode("utf-8")
     for i in token(statements):
-        print "%s\t\t%s" % (i[0],i[1])
+        print "%s\t\t%s" % (i[0], i[1])
     l = []
     l.append("。".decode("utf-8"))
     print l
-
-
-        
--- segment/ZooSegment.py
+++ segment/ZooSegment.py
@@ -3,39 +3,39 @@
 
 from Trie import Trie
 import filetutil
-from segment import PreSegment
+import PreSegment
 
 
 class Segment(object):
-    
-    
+
+
     def segment(self,words):
         pass
 
 
 class SMM(Segment):
-    
+
     _dict = Trie()
-    
+
     def __init__(self , dictpath , maxlength=8):
         self.dictpath = dictpath
         self.maxlength = maxlength
         self._load_word_dict()
-    
+
     def _load_word_dict(self ):
         contents = filetutil.read_file_strip(self.dictpath)
         for word in contents:
             wordarry = word.split()
             self._dict.add(wordarry[0], wordarry[1])
-    
-    
+
+
     def signal_word_in(self,words):
         count = 0
         for word in words:
             if len(word) == 1:
                 count = count + 1
         return count
-    
+
     def segment(self,words):
         _segmentwords = []
         words = words.decode("utf-8")
@@ -46,11 +46,11 @@ class SMM(Segment):
             else:
                 _segmentwords.append(_words)
         return _segmentwords
-    
-    #解析中文分词 
+
+    #解析中文分词
     def _znsegment(self,words):
         pass
-    
+
     #
     def _segment(self,words):
         if words:
@@ -59,14 +59,14 @@ class SMM(Segment):
             else:
                 return  words[0]
         return  ""
-        
-             
-        
-        
-class FMM(SMM):
-    
 
-        
+
+
+
+class FMM(SMM):
+
+
+
     def _znsegment(self, words):
         _result = []
         if words and len(words):
@@ -80,14 +80,14 @@ class FMM(SMM):
                 while  rindex > 1:
                     if self._dict.search(token[:rindex]):
                         break
-                    rindex = rindex - 1 
+                    rindex = rindex - 1
                 _result.append(token[:rindex])
                 substring = substring[len(token[:rindex]):]
         return _result
 
 
 class RMM(SMM):
-    
+
     def _znsegment(self, words):
         _result = []
         if words and len(words):
@@ -105,20 +105,20 @@ class RMM(SMM):
                 _result.append(token[lindex:])
                 substring = substring[:-len(token[lindex:])]
             _result.reverse()
-            
+
         return _result
 
 class BMM(SMM):
-    
+
     _fm = None
     _rm = None
-    
+
     def __init__(self,dictpath=8, maxlength=8):
         SMM.__init__(self, dictpath, maxlength)
         self._fm = FMM(dictpath,maxlength)
         self._rm = RMM(dictpath,maxlength)
-        
-        
+
+
     def segment(self, words):
         fwords = self._fm.segment(words)
         rwords = self._rm.segment(words)
@@ -135,16 +135,16 @@ class BMM(SMM):
                 return fwords
             else:
                 return rwords
-    
 
-        
-            
-        
-        
-        
-        
+
+
+
+
+
+
+
 if __name__ == "__main__":
     seg = FMM("dict.txt")
-    
+
     print " ".join(seg.segment("如果不肯换位体验，能不能让他们失去位子？！否则他们永远不会懂得权力来自人民。 //@人民日报:【想听真话摸实情，不如换位体验】网友建议：请民航部门领导以普通乘客身份，体验飞机晚点的烦恼…...感同身受，换位思考，还有哪些地方需要领导去体验？欢迎补充〜"))
-        
+
