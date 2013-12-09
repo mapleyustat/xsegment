43ca7251847394d012635579968c172d47ec439a
lixuze <aihai2800@126.com>
2013-12-09 10:50:34 +0800

    添加正则

--- segment/PreSegment.py
+++ segment/PreSegment.py
@@ -12,14 +12,17 @@ REGX_ARRY = [(
     'URL', ur"((https?|ftp|news):\/\/)?([a-z]([a-z0-9\-]*[\.。])+([a-z]*)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&]*)?)?(#[a-z][a-z0-9_]*)?"),
     ('SIGN', r"[\.\=、，!！_?？\[\]【】：:。“”…\"〜]+"),
     ('AT', ur"(//)?@[\u4e00-\u9fa5a-z0-9]+"),
+    ('IP' , ur'((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)'),
     ('FLOAT', ur"\d+\.\d+"),
     ('INTEGER', ur"[0-9]+"),
     ('ZN', ur"[\u4e00-\u9fa5]+"),
-    ('EN', ur"[a-zA-Z]+")]
+    ('EN', ur"[a-zA-Z]+"),
+    ('MAIL',ur'[a-z]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[.][a-z]{2,3}([.][a-z]{2})?'),
+    ]
 
 
 WORD_EXTRACT_REGX = ur'|'.join(ur'(?P<%s>%s)' % (pair[0],pair[1]) for pair in REGX_ARRY)
-WORD_EXTRACT = re.compile(WORD_EXTRACT_REGX, re.U).finditer
+WORD_EXTRACT = re.compile(WORD_EXTRACT_REGX, re.U|re.IGNORECASE).finditer
 
 
 
@@ -38,7 +41,7 @@ def token(words):
 
 
 if __name__ == "__main__":
-    statements = """五一假期拼凑了[。]“一个Google Analytics自动标记工具，我在博客里对这个工具进行简单的功能介绍并分享给有需要的朋友们使用。文章结尾附有工具下载地址。希望帮助大家减轻Google Analytics代码定制时的工作量。
+    statements = """五一假期拼凑了[。]“一个Google Analytics自动标记工具，我在博客里对这个工具进行简单的功能介绍并分享给有需要的朋友们使用。文章结尾附有工具下载地址。希望帮助大家减轻Google Analytics代码定制时的工作量。192.168.33
 
 Read more: http://bluewhale.cc/tag/google-analytics#ixzz2cHtxfYjR""".decode("utf-8")
     for i in token(statements):
--- segment/ZooSegment.py
+++ segment/ZooSegment.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 #!/usr/bin/env python
 
 from Trie import Trie
@@ -8,35 +8,35 @@ import PreSegment
 
 class Segment(object):
 
-
-    def segment(self,words):
+    def segment(self, words):
         pass
 
 
 class SMM(Segment):
 
-    _dict = Trie()
+    word_dict = Trie() #词典树
 
-    def __init__(self , dictpath , maxlength=8):
-        self.dictpath = dictpath
+
+    def __init__(self, dictpath, maxlength=8):
+        self.word_dictpath = dictpath
         self.maxlength = maxlength
         self._load_word_dict()
 
-    def _load_word_dict(self ):
-        contents = filetutil.read_file_strip(self.dictpath)
-        for word in contents:
-            wordarry = word.split()
-            self._dict.add(wordarry[0], wordarry[1])
+    #载入词典
+    def _load_word_dict(self):
+        with open(self.word_dictpath) as f:
+            [self.word_dict.add(word[0], word[1])
+             for word in [line.strip().split() for line in f.readlines()]]
 
 
-    def signal_word_in(self,words):
+    def signal_word_in(self, words):
         count = 0
         for word in words:
             if len(word) == 1:
                 count = count + 1
         return count
 
-    def segment(self,words):
+    def segment(self, words):
         _segmentwords = []
         words = words.decode("utf-8")
         for word in PreSegment.token(words):
@@ -47,25 +47,21 @@ class SMM(Segment):
                 _segmentwords.append(_words)
         return _segmentwords
 
-    #解析中文分词
-    def _znsegment(self,words):
+    # 解析中文分词
+    def _znsegment(self, words):
         pass
 
     #
-    def _segment(self,words):
+    def _segment(self, words):
         if words:
             if words[1] == 'ZN':
                 return self._znsegment(words[0])
             else:
-                return  words[0]
-        return  ""
-
-
+                return words[0]
+        return ""
 
 
 class FMM(SMM):
-
-
 
     def _znsegment(self, words):
         _result = []
@@ -73,12 +69,12 @@ class FMM(SMM):
             substring = words
             while len(substring):
                 subindex = self.maxlength
-                if subindex >len(substring):
+                if subindex > len(substring):
                     subindex = len(substring)
                 token = substring[:subindex]
                 rindex = len(token)
-                while  rindex > 1:
-                    if self._dict.search(token[:rindex]):
+                while rindex > 1:
+                    if self.word_dict.search(token[:rindex]):
                         break
                     rindex = rindex - 1
                 _result.append(token[:rindex])
@@ -94,12 +90,12 @@ class RMM(SMM):
             substring = words
             while len(substring):
                 subindex = self.maxlength
-                if subindex >len(substring):
+                if subindex > len(substring):
                     subindex = len(substring)
                 token = substring[-subindex:]
                 lindex = 0
-                while  lindex < (len(token) - 1):
-                    if self._dict.search(token[lindex:]):
+                while lindex < (len(token) - 1):
+                    if self.word_dict.search(token[lindex:]):
                         break
                     lindex = lindex + 1
                 _result.append(token[lindex:])
@@ -108,16 +104,16 @@ class RMM(SMM):
 
         return _result
 
+
 class BMM(SMM):
 
     _fm = None
     _rm = None
 
-    def __init__(self,dictpath=8, maxlength=8):
+    def __init__(self, dictpath=8, maxlength=8):
         SMM.__init__(self, dictpath, maxlength)
-        self._fm = FMM(dictpath,maxlength)
-        self._rm = RMM(dictpath,maxlength)
-
+        self._fm = FMM(dictpath, maxlength)
+        self._rm = RMM(dictpath, maxlength)
 
     def segment(self, words):
         fwords = self._fm.segment(words)
@@ -128,7 +124,8 @@ class BMM(SMM):
         elif minlen < 0:
             return fwords
         else:
-            diff_signal_word_num = self.signal_word_in(fwords) -  self.signal_word_in(rwords)
+            diff_signal_word_num = self.signal_word_in(
+                fwords) - self.signal_word_in(rwords)
             if diff_signal_word_num > 0:
                 return rwords
             elif diff_signal_word_num < 0:
@@ -137,15 +134,7 @@ class BMM(SMM):
                 return rwords
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
     seg = FMM("dict.txt")
 
     print " ".join(seg.segment("如果不肯换位体验，能不能让他们失去位子？！否则他们永远不会懂得权力来自人民。 //@人民日报:【想听真话摸实情，不如换位体验】网友建议：请民航部门领导以普通乘客身份，体验飞机晚点的烦恼…...感同身受，换位思考，还有哪些地方需要领导去体验？欢迎补充〜"))
-
-
