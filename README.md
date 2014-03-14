cb0b5ae308b7932549d138a4129820571d8aa687
lixuze <intoblack86@gmail.com>
2014-03-14 21:09:08 +0800

    添加情感词的简单计算

--- README.md
+++ README.md
@@ -27,3 +27,20 @@ zoo-segment 中文分词python分词
           print p.zh2pinyin('我爱a' , '#') # wo#ai#a
 
 
+情感极性简单分析
+---------------------
+:::python
+
+
+     from psentiment import SentimentTrie
+
+     sentiment = SentimentTrie()
+     print sentiment.get_word_sentiment('断章取义') # 返回值 -1.2 情感为负
+     print sentiment.get_words_sentiment(['我' , '喜欢' , '你']) #[('\xe6\x88\x91', 1.7499999999999998), ('\xe5\x96\x9c\xe6\xac\xa2', 1.4310722100656499), ('\xe4\xbd\xa0', -0.7)] 返回每个词的极值
+     print sentiment.get_sentence_sentiment(['我' , '喜欢' , '你']) # 返回2.48107221007 情感为积极
+     print sentiment.get_sentence_sentiment(['我' , '恨' , '你']) #-0.4392 情感为消极
+
++ 计算情感词的极值 ， 现在简单的实现
++ 必须要加入否定词概念 
++ 情感极性 ， 推荐svm训练
+
---
+++ segment/psentiment.py
@@ -0,0 +1,80 @@
+#coding=utf-8
+#!/usr/bin/env python
+
+
+
+from Trie import Trie
+
+def singleton(cls, *args, **kw):   
+    instances = {}   
+    def singleton_instance():   
+        if cls not in instances:   
+            instances[cls] = cls(*args, **kw)   
+        return instances[cls]   
+    return singleton_instance
+
+
+
+@singleton
+class SentimentTrie(object):
+
+
+	__sentiment_trie = Trie()
+
+	def __init__(self , dict_path = './dict/sentiment_word.txt' , split_word = '\t'):
+		self.__load(dict_path , split_word)
+
+
+
+	def __load(self , dict_path , split_word = '\t'):
+		print 'load'
+		with open(dict_path) as __file_handle:
+			for line in __file_handle.readlines():
+				line = line.strip()
+				sentiment_word = line.split(split_word)
+				if len(sentiment_word) >= 2:
+					self.__sentiment_trie.add(sentiment_word[0].decode('utf-8') , float(sentiment_word[1]))
+
+
+	def get_word_sentiment(self , word):
+		if word:
+			if not isinstance(word, (str , unicode)):
+				word = word.decode('utf-8')
+		__value =  self.__sentiment_trie.find(word.decode('utf-8'))
+		if __value and isinstance(__value , float):
+			return __value
+		else:
+			return 0.
+
+
+	def get_words_sentiment(self , word_list = []):
+		__sentiment_list = []
+		if word_list and len(word_list) > 0:
+			__sentiment_list.extend([(word , self.get_word_sentiment(word)) for word in word_list ])
+		return __sentiment_list
+
+
+	def get_sentence_sentiment(self , word_list = []):
+		sentiment_point = 0.
+		for __point in self.get_words_sentiment(word_list):
+			sentiment_point += __point[1]
+		return sentiment_point
+
+
+
+
+
+if __name__ == '__main__':
+	sentiment = SentimentTrie()
+	print sentiment.get_word_sentiment('断章取义')
+	print sentiment.get_words_sentiment(['我' , '喜欢' , '你'])
+	print sentiment.get_sentence_sentiment(['我' , '喜欢' , '你'])
+	print sentiment.get_sentence_sentiment(['我' , '恨' , '你'])
+
+
+
+
+
+
+
+
