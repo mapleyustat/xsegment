116e48f37728756515c1f7926a495a19ba3491b5
lixuze <intoblack86@gmail.com>
2014-05-07 23:03:36 +0800

    添加转移矩阵 构建  将代码优化

--- xsegment/hmm.py
+++ xsegment/hmm.py
@@ -9,14 +9,27 @@ sys.setdefaultencoding('utf-8')
 
 
 state = defaultdict(int)
-start_state = {'s': 0, 'b': 0}
-
+# 开始状态矩阵构建
+start_state = {'s': 0, 'b': 0, 'm': 0, 'e': 0}
+# 隐藏状态转移
 transition_probability = {
     's': {'s': 0,  'b': 0, 'm': 0, 'e': 0}, 'b': {'s': 0,  'b': 0, 'm': 0, 'e': 0},
     'm': {'s': 0,  'b': 0, 'm': 0, 'e': 0}, 'e': {'s': 0,  'b': 0, 'm': 0, 'e': 0}}
-emission_probability = {'s': {}, 'e': {}, 'b': {}, 'm': {}}
+# 隐藏状态下各个观察状态发生频率
+emission_probability = {'s': defaultdict(int), 'e': defaultdict(
+    int), 'b': defaultdict(int), 'm': defaultdict(int)}
 
 
+def save_state():
+    with open('start_state.dat', 'w') as f:
+        f.write(json.dumps(start_state))
+    with open('emission_probability.dat', 'w') as f:
+        f.write(json.dumps(emission_probability))
+    with open('transition_probability.dat', 'w') as f:
+        f.write(json.dumps(transition_probability))
+
+
+# 打开一个文件
 with open('pku_training.utf8') as f:
     for line in f.readlines():
         text_arry = line.strip().split(' ')
@@ -29,23 +42,24 @@ with open('pku_training.utf8') as f:
             word = word.decode('utf-8')
             word = word.strip()
             if len(word) == 1:
-                word_label.append('s')
-                state['s'] = state['s'] + 1
+                word_label.append(('s', word))
+
             elif len(word) == 2:
-                word_label.append('b')
-                word_label.append('e')
-                state['b'] += 1
-                state['e'] += 1
+                word_label.append(('b', word[0]))
+                word_label.append(('e', word[1]))
             elif len(word) > 2:
-                word_label.append('b')
-                for i in range(len(word[1:-2])):
-                    word_label.append('m')
-                word_label.append('e')
-                state['b'] += 1
-                state['m'] += len(word[1:-2])
-                state['e'] += 1
+                word_label.append(('b', word[0]))
+                for i in range(len(word[1:-1])):
+                    word_label.append(('m', word[i + 1]))
+                word_label.append(('e', word[-1]))
+
         for i in range(len(word_label) - 1):
-            transition_probability[word_label[i]][word_label[i + 1]] += 1
-print state
-print start_state
-print transition_probability
+            transition_probability[word_label[i][0]][word_label[i + 1][0]] += 1
+        for i in range(len(word_label)):
+            state[word_label[i][0]] += 1
+            emission_probability[word_label[i][0]][word_label[i][1]] += 1
+# print state
+# print start_state
+# print transition_probability
+# print json.dumps(emission_probability)
+save_state()
