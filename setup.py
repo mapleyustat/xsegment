2e1aab29f88ce7c106427342a26d0a3c20229b64
lixuze <intoblack86@gmail.com>
2014-05-10 01:13:08 +0800

    修改setup 文件 , 还有 textrank 也要准备编写

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 kw = dict(
     name='pynettool',
-    version='0.0.3',
+    version='0.1.0',
     description='chinese words segment use regular ',
     author='intoblack',
     author_email='intoblack86@gmail.com',
