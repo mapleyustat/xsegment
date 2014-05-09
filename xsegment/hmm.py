e3c9f7c60b5687dfb16860690f1c3514fe00ae03
lixuze <intoblack86@gmail.com>
2014-05-10 00:15:46 +0800

    添加训练预料代码

--- xsegment/hmm.py
+++ xsegment/hmm.py
@@ -8,6 +8,74 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
+
+class HSegment(object):
+	__start_state = None
+	__emission_probability = None
+	__transition_probability = None
+	__states = ['s', 'm', 'b', 'e']
+
+	def __init__(self, model='./'):
+		self.__load(model)
+
+	def __load(self, path):
+		if path:
+			if not path.endswith('/'):
+				path = path + '/'
+		with open('%s%s' % (path, 'start_state.dat')) as f:
+			start_state = json.loads(f.readline())
+		with open('%s%s' % (path, 'emission_probability')) as f:
+			__emission_probability = json.loads(f.readline())
+		with open('%s%s' % (path, 'transition_probability.dat')) as f:
+			__transition_probability = json.loads(f.readline())
+
+	def __viterbi(self, obs):
+		'''
+		维特比算法 摘自wiki 维特比算法
+		'''
+		V = [{}]
+        path = {}
+        for y in self.__states:
+            V[0][y] = self.__start_state[y] * \
+                self.__emission_probability[y][obs[0]]
+            path[y] = [y]
+        for t in range(1, len(obs)):
+            V.append({})
+            newpath = {}
+            for y in self.__states:
+                (prob, state) = max(
+                    [(V[t - 1][y0] * self.__transition_probability[y0][y] * self.__emission_probability[y][obs[t]], y0) for y0 in self.__states])
+                V[t][y] = prob
+                newpath[y] = path[state] + [y]
+            path = newpath
+        (prob, state) = max([(V[len(obs) - 1][y], y) for y in self.__states])
+        return (prob, path[state])
+    
+    def segment(self , sentence):
+    	if sentence :
+    		if not isinstance(sentence , 'unicode'):
+    			sentence = sentence.decode('utf-8')
+    	__obs = self.__viterbi(sentence)[1]
+        word = []
+        __index = 0
+        __size = len(sentence)
+        while __index < __size:
+            if __obs[__index] == 's':
+                word.append(sentence[__index])
+                __index = __index + 1
+            elif __obs[__index] == 'b':
+                __word = []
+                while __obs[__index] != 'e':
+                    __word.append(sentence[__index])
+                    __index = __index + 1
+                __word.append(sentence[__index])
+                word.append(''.join(__word))
+                __index = __index + 1
+            else:
+                print __obs[__index]
+        return word
+
+
 class trainHmm(object):
     state = defaultdict(float)
     __states = ['s', 'm', 'b', 'e']
@@ -25,11 +93,11 @@ class trainHmm(object):
     word_state = set()
 
     def save_state(self):
-        with open('__start_state.dat', 'w') as f:
+        with open('start_state.dat', 'w') as f:
             f.write(json.dumps(self.__start_state))
-        with open('__emission_probability.dat', 'w') as f:
+        with open('emission_probability.dat', 'w') as f:
             f.write(json.dumps(self.__emission_probability))
-        with open('__transition_probability.dat', 'w') as f:
+        with open('transition_probability.dat', 'w') as f:
             f.write(json.dumps(self.__transition_probability))
 
     def add_line(self, line):
@@ -69,9 +137,9 @@ class trainHmm(object):
         with open(file_name) as f:
             for line in f.readlines():
                 self.add_line(line)
-        self.__translte()
+        self.translte()
 
-    def __translte(self):
+    def translte(self):
         '''
         有状态数量转换为概率
         '''
@@ -98,52 +166,38 @@ class trainHmm(object):
                 self.__emission_probability[__hide][word] = (
                     self.__emission_probability[__hide][word] + 1) / self.state[__hide]
 
-    def __viterbi(self, obs):
-        V = [{}]
-        path = {}
-        for y in self.__states:
-            V[0][y] = self.__start_state[y] * \
-                self.__emission_probability[y][obs[0]]
-            path[y] = [y]
-        for t in range(1, len(obs)):
-            V.append({})
-            newpath = {}
-            for y in self.__states:
-                (prob, state) = max(
-                    [(V[t - 1][y0] * self.__transition_probability[y0][y] * self.__emission_probability[y][obs[t]], y0) for y0 in self.__states])
-                V[t][y] = prob
-                newpath[y] = path[state] + [y]
-            path = newpath
-        (prob, state) = max([(V[len(obs) - 1][y], y) for y in self.__states])
-        return (prob, path[state])
-
-    def segment(self, sentence):
-        sentence = sentence.decode('utf-8')
-        __obs = self.__viterbi(sentence)[1]
-        word = []
-        __index = 0
-        __size = len(sentence)
-        while __index < __size:
-            if __obs[__index] == 's':
-                word.append(sentence[__index])
-                __index = __index + 1
-            elif __obs[__index] == 'b':
-                __word = []
-                while __obs[__index] != 'e':
-                    __word.append(sentence[__index])
-                    __index = __index + 1
-                __word.append(sentence[__index])
-                word.append(''.join(__word))
-                __index = __index + 1
-            else:
-                print __obs[__index]
-        return word
 
 
+
+    
 if __name__ == '__main__':
-    t = trainHmm()
-    t.train('pku_training.utf8')
+	h = HSegment()
+	h.segment('我的分词软件!')
+    # import os 
+    # import re 
+
+    # t = trainHmm()
+
+    # word = re.compile('/[a-z]+\s?')
+    # diff = set()
+    # for file_name in os.listdir("data/"):
+    # 	file_path = '%s%s' % ('data/' , file_name)
+    # 	with open(file_path) as f:
+    # 		content = f.readlines()
+    # 		try:
+    # 		    wd = content[2].split('：')[1].strip()
+    # 		except Exception,e:
+    # 			print file_path , e
+    # 		for line in content[6:]:
+    # 			line = line.strip().replace('[%s]' % wd , wd).split("\t")[2]
+    # 			if line in diff:
+    # 				continue
+    # 			diff.add(line)
+    # 			t.add_line(' '.join(word.split(line)))
+
+    # t.train('pku_training.utf8')
+    # t.translte()
     # print t.segment([u'我', u'爱', u'我', u'的', u'祖', u'国', u'。'])
-    print ' '.join(t.segment(u'南京市长江大桥今天竣工！'))
-    print ' '.join(t.segment(u'李旭泽，想投奔搜狐新闻客户端可以联系我哈，我直送他们领导老蔡，哈哈。。'))
-    t.save_state()
+    # print ' '.join(t.segment(u'南京市长江大桥今天竣工！'))
+    # print ' '.join(t.segment(u'李旭泽，想投奔搜狐新闻客户端可以联系我哈，我直送他们领导老李，哈哈。。'))
+    # t.save_state()
