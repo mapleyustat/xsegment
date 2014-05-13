edd1e70cc0a88402c6a8dad1a74f5a561ae2b928
lixuze <intoblack86@gmail.com>
2014-05-13 22:14:06 +0800

    textrank 实现代码  数组 实现 ， 优化原有的算法

--- xsegment/textrank.py
+++ xsegment/textrank.py
@@ -18,27 +18,59 @@ class TextRank(object):
         pass
 
     def extractWord(self, sententce):
+    	'''
+    	关键词抽取 ：
+    	textrank 算法实现
+    	'''
         if sententce and len(sententce) > 0:
             if isinstance(sententce, (str, unicode)):
                 sententce = self.split_regx(sententce)
             elif not isinstance(sententce, (list, tuple)):
                 raise Exception, 'type erro'
+        sententce = [ word for word in sententce  if chinese(word.decode('utf-8'))]
         word_map = self.__create_word_map(sententce)
         word_len = len(set(word_map))  # 词数
         word_arry = TextRank.createList(word_len, word_len)
         for i in range(1, len(sententce)):
+                # 建立窗口 ， 一个词投给另个词
             word_arry[word_map[sententce[i]]][word_map[sententce[i - 1]]] += 1
 
         # 生成初始化矩阵
-        for j in range(word_len):
-            noZero = 0
-            for i in range(word_len):
-                if word_arry[i][j] != 0:
-                    noZero += 1
-            if noZero != 0:
-                for i in range(word_len):
-                    word_arry[i][j] /= float(noZero)
-        print word_arry
+        # for j in range(word_len):
+        #     noZero = 0
+        #     for i in range(word_len):
+        #         if word_arry[i][j] != 0:
+        #             noZero += 1
+        #     if noZero != 0:
+        #         for i in range(word_len):
+        #             word_arry[i][j] /= float(noZero)
+        score = self.dopagerank(word_arry)
+        text_rank = [(__key, score[__val])
+                     for __key, __val in word_map.items()]
+        return sorted(text_rank, key=lambda x: x[1], reverse=True)
+
+    def dopagerank(self, word_arry, iter=30, d = 0.85, diff=0.0001):
+        max_diff = 0.
+        score = [0.1 for _ in range(len(word_arry))]
+        score_back = [0.1 for _ in range(len(word_arry))]
+        for count in range(iter):
+            for c in range(len(score)):
+                score_back[c] = score[c]
+            for i in range(len(word_arry)):
+                __point = 0.
+                for j in range(len(word_arry[i])):
+                    __out = 0.  # 出链数
+                    if word_arry[i][j] != 0.:
+                        for o in range(len(word_arry)):
+                            __out += word_arry[o][j]
+                        if __out != 0.:
+                        	__point += score[i] / __out
+                score[i] = 1 - d + d * __point
+                if abs(score[i] - score_back[j]) > max_diff:
+                    max_diff = abs(score[i] - score_back[j])
+        if max_diff < diff:
+            return score
+        return score
 
     def __create_word_map(self, sententce):
         word_map = {}
@@ -64,7 +96,7 @@ class TextRank(object):
 
 class TextRank1(object):
 
-    """docstring for TextRank1"""
+    """"""
 
     def __init__(self):
         pass
@@ -107,15 +139,17 @@ class TextRank1(object):
         '''
         if isinstance(word_window, dict):
             scoreDict = defaultdict(float)
+            tmp = defaultdict(dict)            
             for __key in word_window.keys():
                 scoreDict[__key] = 1.
             for c in range(iter_count):
                 max_diff = 0.
-                tmp = scoreDict.copy()  # everytime
+                for __key in scoreDict.keys():
+                	tmp[__key] = scoreDict [__key]
                 for __key, __val in word_window.items():
                     score = 0.
-                    for __word in __val:
-                        __out = 0
+                    for __word in __val:  # 循环每个窗口词
+                        __out = 0  # 初练数目
                         if word_window.has_key(__word):
                             __out = len(word_window[__word])
                         if __word == __key or __out == 0:
@@ -125,7 +159,8 @@ class TextRank1(object):
                     __diff = abs(tmp[__key] - scoreDict[__key])
                     if max_diff < __diff:
                         max_diff = __diff
-                scoreDict = tmp.copy()
+                for __key in scoreDict.keys():
+                	scoreDict[__key] = tmp [__key]
                 if max_diff <= diff:
                     break
             return scoreDict
@@ -143,22 +178,23 @@ class TextRank1(object):
             return l
 
 if __name__ == '__main__':
-    # x = TextRank()
-    # print TextRank.createList(3, 4)[2][1]
-    # x.extractWord('我 说 你 应该  知道 我 , 你 说 你 知道 什么')
+    x = TextRank()
+    x.extractWord('我 说 你 应该  知道 我 , 你 说 你 知道 什么')
     from xsegment.ZooSegment import FMM
     h = FMM()
+    p = x.extractWord(
+        h.segment("""   新华网内罗毕5月11日电（记者张艺 俞铮）国务院总理李克强11日上午在内罗毕国家宫与肯尼亚总统肯雅塔、乌干达总统穆塞韦尼、卢旺达总统卡加梅、南苏丹总统基尔等东非地区国家领导人，以及坦桑尼亚、布隆迪、非洲开发银行代表共同出席肯尼亚蒙巴萨至内罗毕铁路项目中肯共同融资协议签字仪式并讲话。该项目将由中国公司承建。李克强说，很高兴在内罗毕同各国领导人相聚一堂共商大计。你们专程前来出席此次签字仪式，表明各国支持建设东非铁路网的共同意愿。国家要发展，交通基础设施要先行。中方愿同各方分享铁路建设方面的技术和经验，积极开展设计、建设、装备、管理、人才培训、项目融资等合作。李克强表示，东非地区乃至非洲实现互联互通，将对非洲国家经济发展起到重要支撑作用。蒙内铁路是中非从次区域合作起步，共同建设非洲高速铁路、高速公路和区域航空三大网络的重大项目，中方将与相关国家及非盟加强沟通协作，充分发挥中非金融机构的融资功能，也欢迎域外第三方机构积极参与，实现互利共赢。东非国家领导人表示，蒙内铁路是肯尼亚近百年来新建的第一条铁路，将进一步完善东非铁路网，增加东非国家的运力，推进东非地区的互联互通和一体化建设，促进各国经济发展。各国感谢中国的帮助和支持，将与中方齐心协力尽早建好这一铁路，造福本地区人民。""") )
+    for i in p:
+        print i[0], i[1]
     k = TextRank1.create_word_window(
         ' '.join(h.segment("""   新华网内罗毕5月11日电（记者张艺 俞铮）国务院总理李克强11日上午在内罗毕国家宫与肯尼亚总统肯雅塔、乌干达总统穆塞韦尼、卢旺达总统卡加梅、南苏丹总统基尔等东非地区国家领导人，以及坦桑尼亚、布隆迪、非洲开发银行代表共同出席肯尼亚蒙巴萨至内罗毕铁路项目中肯共同融资协议签字仪式并讲话。该项目将由中国公司承建。李克强说，很高兴在内罗毕同各国领导人相聚一堂共商大计。你们专程前来出席此次签字仪式，表明各国支持建设东非铁路网的共同意愿。国家要发展，交通基础设施要先行。中方愿同各方分享铁路建设方面的技术和经验，积极开展设计、建设、装备、管理、人才培训、项目融资等合作。李克强表示，东非地区乃至非洲实现互联互通，将对非洲国家经济发展起到重要支撑作用。蒙内铁路是中非从次区域合作起步，共同建设非洲高速铁路、高速公路和区域航空三大网络的重大项目，中方将与相关国家及非盟加强沟通协作，充分发挥中非金融机构的融资功能，也欢迎域外第三方机构积极参与，实现互利共赢。东非国家领导人表示，蒙内铁路是肯尼亚近百年来新建的第一条铁路，将进一步完善东非铁路网，增加东非国家的运力，推进东非地区的互联互通和一体化建设，促进各国经济发展。各国感谢中国的帮助和支持，将与中方齐心协力尽早建好这一铁路，造福本地区人民。""") ), 7)
     c = TextRank1.create_word_window(h.segment('''海外网5月12日电 据杭州警方12日对余杭中泰事件发布两份通报，通报称，余杭中泰事件中53名犯罪嫌疑人被依法刑拘，11名违法犯罪嫌疑人主动向警方投案。杭州警方对7名网上散布谣言违法人员作出行政拘留处罚。通报《余杭中泰事件中53名犯罪嫌疑人被依法刑拘 11名违法犯罪嫌疑人主动向警方投案 5月10日，在少数不法分子的煽动和蛊惑下，余杭中泰及附近地区人员规模性聚集，封堵02省道和杭徽高速公路，一度造成交通中断，一些不法分子甚至趁机打砸、损坏车辆，围攻殴打执法民警和无辜群众。经公安机关调查，迅速将一批涉嫌聚众扰乱公共秩序、妨碍公务和寻衅滋事的犯罪嫌疑人抓获归案。公安机关已对53名涉嫌犯罪的嫌疑人刑事拘留。犯罪嫌疑人裘某某，现年43岁，临安市人。经审讯，裘某某如实供述了自己的犯罪事实。5月10日，其在当天现场聚集人群中，面对着执勤民警高喊：“大家冲上去，打死他们”，并用石块砸向现场民警。之后，他还爬上杭徽高速公路阻断交通。犯罪嫌疑人何某某，24岁，四川人，被警方抓获后承认伙同张某，用石块打砸警车。现裘某某、何某某已被警方刑事拘留。
 5月11日，余杭区人民法院、余杭区人民检察院、杭州市公安局余杭分局、杭州市余杭区司法局《关于敦促违法犯罪人员自首的通告》发布后，一批违法犯罪嫌疑人慑于法律的威严，在亲友的规劝下，主动向公安机关投案。截至发稿时，已有11名涉案人员向警方投案。25岁的李某， 5月10日涉嫌在“5.10事件”现场袭警，案发后在逃。警方将其上网追逃，5月11日下午2时许，李某迫于压力，主动到公安机关投案自首，承认了自己殴打民警的犯罪事实，并对自己的行为感到非常后悔，愿意承担法律责任。公安机关鉴于其主动投案自首，如实交代自己的犯罪行为，知罪悔罪，决定予以取保候审。
 公安机关重申，凡在此次事件中实施堵塞交通、毁坏公私财物、伤害他人、制造传播谣言等违法犯罪行为的人员，必须主动到公安司法机关投案自首，如实交代自己的违法犯罪行为，争取从宽处理。对拒不投案自首，潜逃或者继续实施违法犯罪行为的，公安司法机关将坚决采取有力措施缉捕归案，依法严惩。通报《杭州警方对7名网上散布谣言违法人员作出行政拘留处罚》如下：
 5月12日，7名在互联网上捏造谣言，散布虚假信息的杭州籍网民张某、许某、陈某、洪某、龚某和浙江台州籍网民冯某、江西籍网民李某、被杭州市公安机关作出行政拘留处罚。5月10日，在余杭中泰地区聚集堵路打砸事件中，张某（女、35岁）在其网名为“静水流深”的微博上，发布“特警用防空盾、催泪瓦斯，如今已经有4个老百姓无辜死亡”的虚假内容。当晚，李某（女、23岁）登陆网名为小灰灰的腾讯微博发帖称“警察就像火车站乱砍人的歹徒一模一样，见人就打，被扔下桥，把人电死，现场哭成一片，实在太恐怖了”。此外，许某（女，28岁）、洪某（男，32岁）、陈某（男，37岁）、冯某（男，25岁）、龚某（女，32岁）相继于5月10日、11日编造“送到医院的伤者已经三个确认死亡，医院封锁消息”、“一个3岁小孩在妈妈怀抱中被特警抢走从立交桥摔死”等谣言在互联网上扩散，引发公众关注，扰乱公共秩序，以上7人均对上述造谣惑众的违法事实供认不讳。公安机关根据《中华人民共和国治安管理处罚法》相关规定，对上述7名人员分别作出行政拘留5-10日的处罚。'''), 3)
-    # for __key , __val in k.items():
-    #     print __key
-    #     print ' '.join(__val)
+    for __key , __val in k.items():
+        print __key
+        print ' '.join(__val)
     scoremap = TextRank1.textrank(c, iter_count=100)
-    l = [(__key, __val) for __key, __val in scoremap.items()]
-    l = sorted(l, key=lambda x: x[1], reverse=True)
-    for i in l:
+    for i in TextRank1.sort_score(scoremap , 8):
         print i[0], i[1]
